7c74714b226b5f7ac30e95974b70ec118a9dd374
Philip1755 <philipneuhaeuser@gmail.com>
2023-03-06 18:23:03 +0100

    PlayerAnimations

--- EscapeUnity/Assets/Scripts/Controller/PlayerController.cs
+++ EscapeUnity/Assets/Scripts/Controller/PlayerController.cs
@@ -3,20 +3,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 
-    private Vector2 movement;
+    private Vector2 movement = Vector2.zero;
 
-    private float xDir, yDir;
+    [SerializeField] private Sprite idle1, idle2, up1, up2, right1, right2, down1, down2, left1, left2;
+    private int spriteIndex = 1;
+    private float animationSpeedInSeconds = .25f;
+    private float spriteTimer = 0;
+
+    private float xDir = 0, yDir = 0;
     [SerializeField] private float speed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         HandleInput();
+        UpdateSprite();
     }
 
     private void FixedUpdate()
@@ -34,5 +42,22 @@ public class PlayerController : MonoBehaviour
     {
         xDir = Input.GetAxisRaw("Horizontal");
         yDir = Input.GetAxisRaw("Vertical");
+    }
+
+    private void UpdateSprite()
+    {
+        if (spriteTimer >= animationSpeedInSeconds)
+        {
+            spriteIndex = spriteIndex == 1 ? 2 : 1;
+            spriteTimer = 0;
+        }
+        else
+            spriteTimer += Time.deltaTime;
+
+        sr.sprite = spriteIndex == 1 ? idle1 : idle2;
+        if (xDir > 0) sr.sprite = spriteIndex == 1 ? right1 : right2;
+        if (xDir < 0) sr.sprite = spriteIndex == 1 ? left1 : left2;
+        if (yDir > 0) sr.sprite = spriteIndex == 1 ? up1 : up2;
+        if (yDir < 0) sr.sprite = spriteIndex == 1 ? down1 : down2;
     }
 }
