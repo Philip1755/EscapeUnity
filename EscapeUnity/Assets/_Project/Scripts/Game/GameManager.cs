c6bc38eda94cca1d22a38e8f20d07c4d981fcb76
Philip1755 <philipneuhaeuser@gmail.com>
2023-03-21 20:44:19 +0100

    Zoom

--- EscapeUnity/Assets/_Project/Scripts/Game/GameManager.cs
+++ EscapeUnity/Assets/_Project/Scripts/Game/GameManager.cs
@@ -1,9 +1,27 @@
+using Cinemachine;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private CinemachineVirtualCamera cam;
+
+    [SerializeField] private float zoomSpeed = .5f;
+    private float orthographicSize = 3.5f;
+
     private void Start()
     {
         AudioManager.Instance.StopMusic();
+    }
+
+    private void Update()
+    {
+        ZoomInOut();
+    }
+
+    private void ZoomInOut()
+    {
+        orthographicSize -= Input.mouseScrollDelta.y * zoomSpeed;
+        orthographicSize = Mathf.Clamp(orthographicSize, 3f, 7f);
+        cam.m_Lens.OrthographicSize = orthographicSize;
     }
 }
